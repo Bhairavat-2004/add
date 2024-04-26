88bafaa29082bad236e6f99d68f5239f5cafc857
Bhairavat-2004 <01fe22bca204@kletech.ac.in>
2024-04-26 04:56:32 -0700

    first commit

---
+++ addoddnumber.java
@@ -0,0 +1,11 @@
+public class addoddnumber {
+public static void main(){
+    int n=0;
+    int sum=0;
+    for(int i=1;i<=n;i+=2)
+    {
+        sum+=i;
+    }
+    System.out.println("Sum of all odd numbers is"+n+":"+sum);
+}
+}
